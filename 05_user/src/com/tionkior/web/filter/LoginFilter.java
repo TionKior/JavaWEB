85098bc2153f923eed249ffb7927ccc3a29cf07b
TionKior <1423304936@qq.com>
2021-04-25 17:31:59 +0800

    feat(05_user): 增加未登录状态下直接访问用户信息跳转到登录页面功能,通过Filter实现

--- 05_user/src/com/tionkior/web/filter/LoginFilter.java
+++ 05_user/src/com/tionkior/web/filter/LoginFilter.java
@@ -33,10 +33,7 @@ public class LoginFilter implements Filter {
                 request.setAttribute("login_msg", "您尚未登录,请登录");
                 request.getRequestDispatcher("/login.jsp").forward(request, resp);
             }
-
-
         }
-
 
         //chain.doFilter(req, resp);
     }
