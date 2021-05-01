c79f91d0c9ab0e2debc87032881c4297e8f2f64c
TionKior <1423304936@qq.com>
2021-05-01 10:57:24 +0800

    feat(Test): 存入数据

--- 06_filter&listener/src/com/tionkior/web/servlettest/ServletTest3.java
+++ 06_filter&listener/src/com/tionkior/web/servlettest/ServletTest3.java
@@ -4,12 +4,18 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet("/ServletTest3")
 public class ServletTest3 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("msg", "hello world");
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add("hello world");
+        arr.add("servlet");
+        arr.add("jsp");
+
+        request.setAttribute("msg", arr);
     }
 
     @Override
