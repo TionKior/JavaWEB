48f8d6a8c38e46cb8a44d37fcbfec35e3e3080b0
TionKior <1423304936@qq.com>
2021-05-12 13:13:12 +0800

    feat: Jedis操作redis中string格式

--- 10_redis/src/com/tionkior/jedis/text/JedisTest.java
+++ 10_redis/src/com/tionkior/jedis/text/JedisTest.java
@@ -1,6 +1,7 @@
 package com.tionkior.jedis.text;
 
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
 
 /**
  * @ClassName : JedisTest
@@ -17,6 +18,33 @@ public class JedisTest {
      */
     @Test
     public void test1() {
+        //1.获取连接
+        Jedis jedis = new Jedis("localhost", 6379);
+        //2.操作
+        jedis.set("username", "zhangsan");
 
+        //3.关闭连接
+        jedis.close();
+    }
+
+    /**
+     * string数据结构操作
+     */
+    @Test
+    public void test2() {
+        //1.获取连接
+        Jedis jedis = new Jedis(); //如果使用空参构造,默认值 "localhost",6379端口
+        //2.操作
+        //存储
+        jedis.set("username", "zhangsan");
+        //获取
+        String username = jedis.get("username");
+        System.out.println(username);
+
+        //可以使用setex()方法存储可以指定过期时间的 key value
+        jedis.setex("activecode", 20, "hehe"); //将activecode：hehe键值对存入redis,并且20秒后自动删除该键值对
+
+        //3.关闭连接
+        jedis.close();
     }
 }
