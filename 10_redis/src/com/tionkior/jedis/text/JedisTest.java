c6cf7cf457c2d4dcc761b6935f0d3d05450f9cc8
TionKior <1423304936@qq.com>
2021-05-12 12:51:54 +0800

    feat: Jedis操作redis快速入门

---
+++ 10_redis/src/com/tionkior/jedis/text/JedisTest.java
@@ -0,0 +1,22 @@
+package com.tionkior.jedis.text;
+
+import org.junit.Test;
+
+/**
+ * @ClassName : JedisTest
+ * @Author : TionKior
+ * @Date : 2021/5/12 12:50
+ * @Version : V1.0
+ * @Description : jedis的测试类
+ */
+
+public class JedisTest {
+
+    /**
+     * 快速入门
+     */
+    @Test
+    public void test1() {
+
+    }
+}
